cf830e10ce62b72f54183bfb9e2a964dec6f7a92
Juhye0k <kyoung1678@naver.com>
2025-01-14 10:18:07 +0900

    feat: 초기 프로젝트 생성

---
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/JobForeignerApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.d138.JobForeigner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobForeignerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobForeignerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/ac/kumoh/d138/JobForeigner/JobForeignerApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.d138.JobForeigner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobForeignerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
