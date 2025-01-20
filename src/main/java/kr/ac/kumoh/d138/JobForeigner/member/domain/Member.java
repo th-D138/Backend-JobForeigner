182f96eb3a04dff5ba8cf988181c35ef52b08265
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-01-20 20:33:34 +0900

    refactor: 회원 엔티티 데이터베이스 매핑 정보 추가

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/member/domain/Member.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/member/domain/Member.java
@@ -1,5 +1,8 @@
 package kr.ac.kumoh.d138.JobForeigner.member.domain;
 
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.AttributeOverrides;
+import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -19,37 +22,52 @@ public class Member {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "member_id", nullable = false)
     private Long id;
 
+    @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "username", nullable = false)
     private String username;
 
+    @Column(name = "password", nullable = false)
     private String password;
 
     @Enumerated(value = EnumType.STRING)
-    private MemberType role;
+    @Column(name = "type", nullable = false)
+    private MemberType type;
 
+    @Column(name = "country_code")
     private String countryCode;
 
+    @Column(name = "phone_number", nullable = false)
     private String phoneNumber;
 
+    @Column(name = "email", nullable = false)
     private String email;
 
     @Enumerated(value = EnumType.STRING)
+    @Column(name = "gender", nullable = false)
     private Gender gender;
 
+    @Column(name = "birth_date", nullable = false)
     private LocalDate birthDate;
 
     @Embedded
+    @AttributeOverrides({
+            @AttributeOverride(name = "address", column = @Column(name = "address", nullable = false)),
+            @AttributeOverride(name = "detailAddress", column = @Column(name = "detail_address", nullable = false)),
+            @AttributeOverride(name = "zipcode", column = @Column(name = "zipcode", nullable = false))
+    })
     private Address address;
 
     @Builder
-    public Member(String name, String username, String password, MemberType role, String countryCode, String phoneNumber, String email, Gender gender, LocalDate birthDate, Address address) {
+    public Member(String name, String username, String password, MemberType type, String countryCode, String phoneNumber, String email, Gender gender, LocalDate birthDate, Address address) {
         this.name = name;
         this.username = username;
         this.password = password;
-        this.role = role;
+        this.type = type;
         this.countryCode = countryCode;
         this.phoneNumber = phoneNumber;
         this.email = email;
