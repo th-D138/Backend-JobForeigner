89913e48f4d908964998a50a1686aa670930e89c
Juhye0k <kyoung1678@naver.com>
2025-03-23 14:54:57 +0900

    feat: 기업 예외정보 추가

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/global/exception/ExceptionType.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/global/exception/ExceptionType.java
@@ -13,8 +13,10 @@ public enum ExceptionType {
     //common
     UNEXPECTED_SERVER_ERROR(INTERNAL_SERVER_ERROR,"C001","예상치 못한 에러 발생"),
     BINDING_ERROR(BAD_REQUEST,"C002","바인딩시 에러 발생"),
-    ESSENTIAL_FIELD_MISSING_ERROR(NO_CONTENT , "C003","필수적인 필드 부재");
+    ESSENTIAL_FIELD_MISSING_ERROR(NO_CONTENT , "C003","필수적인 필드 부재"),
 
+    //User
+    NOT_FOUND_USER(NOT_FOUND,"U001","해당하는 유저를 찾을 수 없습니다.");
     private final HttpStatus status;
     private final String code;
     private final String message;
