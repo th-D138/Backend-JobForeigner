bd532779827f8215feb607d91b5cf275d7ea56d6
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-04-01 11:20:38 +0900

    feat: 예외 메시지 수정 및 ObjectMapper 삭제해 기본 생성 ObjectMapper 사용

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/global/exception/ExceptionType.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/global/exception/ExceptionType.java
@@ -17,14 +17,15 @@ public enum ExceptionType {
 
     // User
     NOT_FOUND_USER(NOT_FOUND, "U001", "사용자를 찾을 수 없습니다."),
-    INVALID_PASSWORD(UNAUTHORIZED, "U002", "비밀번호가 일치하지 않습니다."),
+    INVALID_USER_INFO(UNAUTHORIZED, "U002", "아이디 또는 비밀번호가 일치하지 않습니다."),
 
     // Security
-    NEED_AUTHORIZED(UNAUTHORIZED, "S001", "토큰이 포함되어 있지 않습니다."),
+    NEED_AUTHORIZED(UNAUTHORIZED, "S001", "인증이 필요합니다."),
     ACCESS_DENIED(FORBIDDEN, "S002", "접근 권한이 없습니다."),
-    JWT_EXPIRED(UNAUTHORIZED, "S003", "토큰이 만료되었습니다."),
-    JWT_INVALID(UNAUTHORIZED, "S004", "토큰이 잘못되었습니다."),
-    JWT_NOT_EXIST(UNAUTHORIZED, "S005", "토큰이 존재하지 않습니다."),
+    JWT_EXPIRED(UNAUTHORIZED, "S003", "인증 정보가 만료되었습니다."),
+    JWT_INVALID(UNAUTHORIZED, "S004", "인증 정보가 잘못되었습니다."),
+    JWT_NOT_EXIST(UNAUTHORIZED, "S005", "인증 정보가 존재하지 않습니다."),
+
     ;
 
     private final HttpStatus status;
