1ba398a9832bbb5074ce9279ec666988ec12be3c
Juhye0k <kyoung1678@naver.com>
2025-04-30 13:09:10 +0900

    feat: 채용공고 엔티티, 타입 설정

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/job/domain/JobPost.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/job/domain/JobPost.java
@@ -1,54 +1,85 @@
 package kr.ac.kumoh.d138.JobForeigner.job.domain;
 
 import jakarta.persistence.*;
+import kr.ac.kumoh.d138.JobForeigner.job.dto.company.request.JobPostRequestDto;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class JobPost {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
     private String description;
 
-    @Column(nullable = false)
     private String location;
 
-    @Column(nullable = false)
-    private String employment_type;
+    @Column(name = "employment_type")
+    private String employmentType;
 
-    @Column(nullable = false)
     private String salary;
 
-    @Column(nullable = false)
     private String career;
 
-    @Column(name="created_at",nullable = false)
+    @CreatedDate
+    @Column(name="created_at")
     private LocalDateTime createAt;
 
-    @Column(name="updated_at",nullable = false)
+    @LastModifiedDate
+    @Column(name="updated_at")
     private LocalDateTime updateAt;
 
-    @Column(name="expiry_at",nullable = false)
+    @Column(name="expiry_at")
     private LocalDateTime expiryAt;
 
-    @Column(nullable = false)
-    private boolean Published;
+    @Enumerated(EnumType.STRING)
+    @Column(name = "published")
+    private JobPostStatus published;
 
-    @Column(nullable = false)
     private String grade;
 
     @ManyToOne
     @JoinColumn(name = "company_id")
     private Company company;
 
+@Builder
+public JobPost(String title, String description, String location, String employmentType, 
+               String salary, String career, JobPostStatus published, String grade, 
+               LocalDateTime expiryAt, Company company) {
+    this.title = title;
+    this.description = description;
+    this.location = location;
+    this.employmentType = employmentType;
+    this.salary = salary;
+    this.career = career;
+    this.published = published;
+    this.grade = grade;
+    this.expiryAt = expiryAt;
+    this.company = company;
+}
+
+    public void updatePost(JobPostRequestDto dto){
+        this.title = dto.getTitle();
+        this.description = dto.getDescription();
+        this.location = dto.getLocation();
+        this.employmentType = dto.getEmployment_type();
+        this.salary = dto.getSalary();
+        this.career = dto.getCareer();
+        this.grade = dto.getGrade();
+        this.expiryAt = dto.getExpiryAt();
+    }
 }
