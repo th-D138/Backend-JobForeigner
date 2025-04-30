02a12a5a8f7137ccc9c2bc8385a22ba72298f78d
kon28289 <kon28289@naver.com>
2025-04-30 19:47:03 +0900

    feat: 프로필 조회 추가

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/member/service/MemberService.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/member/service/MemberService.java
@@ -25,14 +25,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class MemberService {
-
-    private final ResumeService resumeService;
 
     private final MemberRepository memberRepository;
     private final RefreshTokenRepository refreshTokenRepository;
@@ -41,6 +47,8 @@ public class MemberService {
     private final RefreshTokenProvider refreshTokenProvider;
 
     private final PasswordEncoder passwordEncoder;
+
+    private final String UPLOAD_DIR = "";
 
     @Transactional
     public JwtPair signUp (SignUpRequest req) {
@@ -109,9 +117,28 @@ public class MemberService {
     public void updateProfileImage(ProfileImageRequest request, Long memberId) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(()->new BusinessException(ExceptionType.MEMBER_NOT_FOUND));
-        String imageUrl = resumeService.storeImage(request.image());
+        String imageUrl = this.storeImage(request.image());
         member.updateProfileImage(imageUrl);
 
+    }
+
+    public String storeImage(MultipartFile file) {
+        try {
+            // 이미지의 확장자 추출
+            String ext = Optional.ofNullable(file.getOriginalFilename())
+                    .filter(f -> f.contains("."))
+                    .map(f -> f.substring(f.lastIndexOf(".")))
+                    .orElse("");
+
+            String savedName = UUID.randomUUID() + ext;
+            Path savePath = Paths.get(UPLOAD_DIR, savedName);
+            Files.createDirectories(savePath.getParent());
+            file.transferTo(savePath.toFile());
+
+            return "/images/" + savedName;
+        } catch (IOException e) {
+            throw new RuntimeException("이미지 저장 실패", e);
+        }
     }
 
 }
