7f309ab24c85d87bf7a1c38997780ec0f6e81171
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-05-12 10:25:32 +0900

    chore: 검증 애너테이션 추가

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/email/dto/request/ResendAuthMailRequest.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/email/dto/request/ResendAuthMailRequest.java
@@ -1,8 +1,11 @@
 package kr.ac.kumoh.d138.JobForeigner.email.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 
 public record ResendAuthMailRequest(
-    @NotBlank String email
+    @NotBlank(message = "이메일은 필수 입력 항목입니다.")
+    @Email(message = "올바른 이메일 형식이 아닙니다.")
+    String email
 ) {
 }
