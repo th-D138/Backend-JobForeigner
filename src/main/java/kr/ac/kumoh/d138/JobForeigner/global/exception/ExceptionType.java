275bbcc758225dc532c2e6e1468729998a0e9bcd
Juhye0k <kyoung1678@naver.com>
2025-07-01 18:42:31 +0900

    feat : 이력서, 알림 관련 예외코드 추가

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/global/exception/ExceptionType.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/global/exception/ExceptionType.java
@@ -41,8 +41,11 @@ public enum ExceptionType {
 
     // Resume
     RESUME_FORBIDDEN(FORBIDDEN,"R001","접근할 수 없는 이력서입니다."),
-    RESUME_NOT_FOUND(NOT_FOUND, "R002", "존재하지 않는 이력서입니다.")
-    ;
+    RESUME_NOT_FOUND(NOT_FOUND, "R002", "존재하지 않는 이력서입니다."),
+
+
+    // Notification
+    NOTIFICATION_NOT_FOUND(NOT_FOUND,"N001","해당하는 알림을 찾을 수 없습니다.");
 
     private final HttpStatus status;
     private final String code;
