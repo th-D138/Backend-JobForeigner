c229e7cf558fda15e1432951e1c406c1d3478a33
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-07-02 20:16:31 +0900

    feat: 회원 탈퇴 후 7일 경과 회원을 물리적 삭제하는 주기 작업 추가

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/JobForeignerApplication.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/JobForeignerApplication.java
@@ -3,9 +3,13 @@ package kr.ac.kumoh.d138.JobForeigner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
 @EnableJpaAuditing
+@EnableScheduling
+@EnableAsync
+@SpringBootApplication
 public class JobForeignerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/global/config/batch/MemberPurgeScheduler.java
@@ -0,0 +1,42 @@
+package kr.ac.kumoh.d138.JobForeigner.global.config.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class MemberPurgeScheduler {
+    private final JobLauncher jobLauncher;
+    private final MemberPurgeConfig batchConfig;
+
+    @Async
+    @Scheduled(cron = "* 30 3 * * *")
+    public void run() {
+        Map<String, JobParameter<?>> parameters = new HashMap<>();
+        parameters.put("time", new JobParameter<>(System.currentTimeMillis(), Long.class));
+        JobParameters jobParameters = new JobParameters(parameters);
+        try {
+            jobLauncher.run(batchConfig.memberPurgeJob(), jobParameters);
+        } catch (JobExecutionAlreadyRunningException |
+                 JobInstanceAlreadyCompleteException |
+                 JobParametersInvalidException |
+                 JobRestartException e
+        ) {
+            log.error(e.getMessage());
+        }
+    }
+}
