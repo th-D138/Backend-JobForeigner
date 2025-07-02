da94ffda271864464f87a9f8cf98684b74e619c7
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-07-02 20:17:23 +0900

    docs: 설정 파일 방식 변경

---
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/global/config/security/CorsProperties.java
@@ -0,0 +1,14 @@
+package kr.ac.kumoh.d138.JobForeigner.global.config.security;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "job-foreigner.cors")
+public class CorsProperties {
+    private List<String> origins;
+}
