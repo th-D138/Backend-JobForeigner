f1d2eb663714cb23bd47cfc4b03fea9555b07844
Juhye0k <kyoung1678@naver.com>
2025-07-10 20:40:53 +0900

    refactor : JobPostRepository 수정 #60

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/job/repository/JobPostRepository.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/job/repository/JobPostRepository.java
@@ -3,8 +3,9 @@ package kr.ac.kumoh.d138.JobForeigner.job.repository;
 import kr.ac.kumoh.d138.JobForeigner.job.domain.JobPost;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface JobPostRepository extends JpaRepository<JobPost, Long> {
-    Optional<JobPost> findByCompanyId(Long companyId);
+    Optional<List<JobPost>> findAllByCompanyId(Long companyId);
 }
