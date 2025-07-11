ab866692bd22dfb8564bd9104d3a7c2e4499cd76
PARK, Yeonjong <yeonjong.park@kumoh.ac.kr>
2025-07-11 20:13:04 +0900

    chore: `@CurrentMemberId`가 Swagger에 표시되지 않도록 수정

--- src/main/java/kr/ac/kumoh/d138/JobForeigner/global/jwt/annotation/CurrentMemberId.java
+++ src/main/java/kr/ac/kumoh/d138/JobForeigner/global/jwt/annotation/CurrentMemberId.java
@@ -1,5 +1,7 @@
 package kr.ac.kumoh.d138.JobForeigner.global.jwt.annotation;
 
+import io.swagger.v3.oas.annotations.Parameter;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -7,6 +9,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
+@Parameter(hidden = true)
 public @interface CurrentMemberId {
     boolean required() default true;
 }
